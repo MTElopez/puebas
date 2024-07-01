60b53fcbca41c3add090e7fee04be4829ec24e31
MTElopez <4matiaslopez4@gmail.com>
2024-07-01 02:49:04 -0300

    avanzo

--- js/validar-form.js
+++ js/validar-form.js
@@ -54,8 +54,45 @@ function validarCampos() {
     }
 }
 
+function enviarDatosBackend(nombre, apellido, edad, email, contraseña) {
+    let datosUsuario = {
+        nombre: nombre,
+        apellido: apellido,
+        edad: parseInt(edad),
+        email: email,
+        contraseña: contraseña,
+    };
+
+    fetch('http://127.0.0.1:5000/api-pc/usuario', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json; charset=UTF-8'
+        },
+        body: JSON.stringify({ datos: datosUsuario })  
+    })
+    .then(response => {
+        if (!response.ok) {
+            throw new Error('Error en la solicitud HTTP: ' + response.status);
+        }
+        return response.json();  
+    })
+    .catch(error => {
+        console.error('Error al enviar datos al backend:', error);
+        
+    });
+}
 
 formulario.addEventListener('submit', evento => {
+
     validarCampos();
+
+    let nombre = document.querySelector('#nombre').value;
+    let apellido = document.querySelector('#apellido').value;
+    let edad = document.querySelector('#edad').value;
+    let email = document.querySelector('#email').value;
+    let contraseña = document.querySelector('#contraseña').value;
+
+    enviarDatosBackend(nombre, apellido, edad, email, contraseña);
+
     evento.preventDefault();
  });
